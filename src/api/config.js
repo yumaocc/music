8fb967524e9433f79152d9eef46888ff5c6c62f8
yumaocc <2310087518@.qq.com>
2022-07-28 00:39:57 +0800

    feat: baseUrl

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
  const instance = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://nodejs-amber.vercel.app",
+    // baseURL: "http://localhost:3000",
 })
 
 instance.interceptors.request.use(function (res) {
